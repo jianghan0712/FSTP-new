29aa3721304cb6e3eeeaadbf9a9ed68b3e5ac5e8
jianghan0712 <jianghan0712@126.com>
2019-03-04 09:30:50 +0800

    遗漏提交

--- fstp-python-3.6/src/strategy/StrategyContainer.py
+++ fstp-python-3.6/src/strategy/StrategyContainer.py
@@ -16,7 +16,7 @@ class StrategyContainer(object):
         'stockcode': [],
         'dbtype' : 'mongodb',
         'dbhost' : 'local',
-        'dbport' : 
+        'dbport' : ''
     }
     
     
